60f1fab5d0dc049ba4f5ee30db661e47562fc30d
jason1987martis <jason1987martis@gmail.com>
2025-11-07 19:52:11 +0530

    removed sesitive info

--- vercel-proxy/api/core.js
+++
@@ -1,6 +0,0 @@
-const { createProxy } = require("../lib/createProxy");
-
-module.exports = createProxy({
-  targetEnv: "CORE_TARGET_URL",
-  allowedMethods: ["GET", "POST", "OPTIONS"]
-});
--- vercel-proxy/api/upload.js
+++
@@ -1,6 +0,0 @@
-const { createProxy } = require("../lib/createProxy");
-
-module.exports = createProxy({
-  targetEnv: "UPLOAD_TARGET_URL",
-  allowedMethods: ["POST", "OPTIONS"]
-});
